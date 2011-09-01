98874e2424dd88152891388442b105b69c4b5e38
omarmojica <omarmojica@gmail.com>
2011-09-01 12:46:52 +0000

    ConvertEmployeesFromXmlToMySQL

---
+++ ConvertEmployeesFromXmlToMySQL/src/pk_util/UtilPojosModelSQL.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pk_util;
+
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory object.
+ *
+ * @author Omar Mojica
+ */
+public class UtilPojosModelSQL {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure("/hibernateSQL.cfg.xml").buildSessionFactory();
+            //SessionFactory sf = new Configuration().configure("/hibernateSQL.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
