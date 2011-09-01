6d1f2fe36f4d44ae33121973bf6baa15878e4563
omarmojica@gmail.com <omarmojica@gmail.com@5c7a6403-7518-4cc8-2e69-14eb1883f0d2>
2011-09-01 12:46:52 +0000

    ConvertEmployeesFromXmlToMySQL

---
+++ trunk/ConvertEmployeesFromXmlToMySQL/src/convertemployeesfromxmltomysql/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package convertemployeesfromxmltomysql;
+
+/**
+ *
+ * @author u19730
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        XMLReader xml = new XMLReader();
+        //xml.main(args);
+    }
+
+}
+
+
